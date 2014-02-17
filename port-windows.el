9c7c09364cceb119f8eeb995f765464cb6c919fb
Sergio de Mingo <sdemingo@gmail.com>
2014-02-17 16:10:47 +0100

    update portable apps windows file

--- port-windows.el
+++ port-windows.el
@@ -11,18 +11,17 @@
 	 "/apps/Java/bin" ";"
 	 "/apps/Portable Python 3.2.5.1/App" ";"
 	 "/apps/strawberry/perl/bin" ";"
-	 "/apps/cygwin/bin" ";"
+	 "/apps/w3m" ";"
 	 (getenv "PATH")
 	 )
 	)
 
-(setq exec-path (append (getenv "PATH")))
-
-;(setq w3m-command "/cygdrive/e/apps/cygwin/bin/w3m.exe")
+;(setq exec-path (append (getenv "PATH")))
 
 ;; Reconfiguro las variables de comando de sistema
 (setq syscmd-file-open "explorer")   ;;de defuns.el
 (setq syscmd-python-run-command "python")  ;;de python-config.el
+
 
 
 (global-set-key (kbd "<f7>")
