6d305915ce4ec2d84f2ce977abdd9e638315e584
Sergio de Mingo <sdemingo@gmail.com>
2014-09-25 20:18:24 +0200

    remove classic way to cut and paste

--- defuns.el
+++ defuns.el
@@ -72,10 +72,10 @@
 
 
 ;; copy and paste by classical way
-(cua-mode t)
-(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
-(transient-mark-mode 1) ;; No region when it is not highlighted
-(setq cua-keep-region-after-copy t)
+;; (cua-mode t)
+;; (setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+;; (transient-mark-mode 1) ;; No region when it is not highlighted
+;; (setq cua-keep-region-after-copy t)
 
 
 ;; Configuro resaltado de línea
