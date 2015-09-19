675ee467734d2cf8474de7695b1b400523235574
Sergio de Mingo <sdemingo@gmail.com>
2015-09-19 10:21:44 +0200

    Add multi-web-mode config file
    To indent js and css embedded in html files

---
+++ web-mode-config.el
@@ -0,0 +1,15 @@
+
+;;
+;;   Configuración de web-mode y multi-web-mode
+;;
+
+
+(require 'multi-web-mode)
+(setq mweb-default-major-mode 'html-mode)
+(setq mweb-tags 
+  '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+    (js-mode  "<script[^>]*>" "</script>")
+    (css-mode "<style[^>]*>" "</style>")))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+(multi-web-global-mode 1)
+
