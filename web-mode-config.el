a9dca14ac822637a98a03ce2b2c8f8c44af8b601
Sergio de Mingo <sdemingo@gmail.com>
2015-11-01 18:02:20 +0100

    add flymake for js

--- web-mode-config.el
+++ web-mode-config.el
@@ -7,9 +7,13 @@
 (require 'multi-web-mode)
 (setq mweb-default-major-mode 'html-mode)
 (setq mweb-tags 
-  '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-    (js-mode  "<script[^>]*>" "</script>")
-    (css-mode "<style[^>]*>" "</style>")))
+      '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+	(js-mode  "<script[^>]*>" "</script>")
+	(css-mode "<style[^>]*>" "</style>")))
 (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
 (multi-web-global-mode 1)
 
+
+
+(require 'flymake-jslint)
+(add-hook 'js-mode-hook 'flymake-jslint-load)
