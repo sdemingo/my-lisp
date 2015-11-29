c12feb1549fda8b16fb1affd86a633cf97858173
Sergio de Mingo <sdemingo@gmail.com>
2015-11-29 09:08:42 +0100

    add goimports and go-mode from elpa

--- go-mode-config.el
+++ go-mode-config.el
@@ -31,10 +31,16 @@
 )
 
 (defun my-go-mode-hook ()
+  ; Use goimports instead of go-fmt
+  (setq gofmt-command "goimports")
+  ; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
   ; Customize compile command to run go build
   (if (not (string-match "go" compile-command))
       (set (make-local-variable 'compile-command)
-           "go generate && go build -v && go test -v && go vet")))
+           "go build -v && go test -v && go vet"))
+  ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump))
 
 
 (defun go-run ()
