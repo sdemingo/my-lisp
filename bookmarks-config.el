03b36c79d50c38c47020b5e2aa087a2afea177fe
Sergio de Mingo <sdemingo@gmail.com>
2017-03-31 17:36:59 +0200

    update bookmarks save function for orgmode

--- bookmarks-config.el
+++ bookmarks-config.el
@@ -21,14 +21,24 @@
       (setq url iurl)
     (setq url (concat prefix iurl))
     )
+
+  (setq tags-list (sort (mapcar 'car (org-global-tags-completion-table)) #'string<))
+  (setq sort-tags-list (mapconcat 'identity tags-list "|"))
+
   (setq title (read-string "Título: "))
   (setq desc (read-string "Descripción: "))
-  (setq tags (read-string "Etiquetas (separadas por \":\"): "))
+  (setq tags (replace-regexp-in-string "[\|.,; ]+" ":" 
+				       (read-string 
+					(format "Etiquetas \n%s: " sort-tags-list))))
+  
   (setq stamp (format-time-string "%Y-%m-%d %a %H:%M"))
   (setq prop (format ":PROPERTIES:\n:CREATED: <%s>\n:END:" stamp))
   (if (= (length tags) 0)
-      (write-region (format "*** [[%s][%s]]\n%s\n%s\n" url title prop desc) nil bookmark-orgmode-file 'append)
-    (write-region (format "*** [[%s][%s]] \t\t\t:%s:\n%s\n%s\n" url title tags prop desc) nil bookmark-orgmode-file 'append)
+      (write-region (format "*** [[%s][%s]]\n%s\n%s\n" url title prop desc) 
+		    nil bookmark-orgmode-file 'append)
+    
+    (write-region (format "*** [[%s][%s]] \t\t\t:%s:\n%s\n%s\n" url title tags prop desc) 
+		  nil bookmark-orgmode-file 'append)
     )
-)
+  )
 
